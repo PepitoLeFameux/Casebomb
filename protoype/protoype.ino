e128cf70a887fa5c24deb4e04bf9dab599c4ff56
CATS <11648807+CoAuToSe@users.noreply.github.com>
2021-09-22 17:38:18 +0200

    update comment

--- protoype/protoype.ino
+++ protoype/protoype.ino
@@ -51,15 +51,16 @@ int numero(float tension)
 int i;
 int n;
 
+// initialisation des variables
 int resultats[]={0,0,0,0,0};
 int combinaison[]={0,0,0,0,0};
 
 int lchiffres[]={0,0,0,0,0,0,0,0};
 int lpair[]={0,0,0,0,0,0,0,0};
 int limpair[]={0,0,0,0,0,0,0,0};
-int impair;
-int pair;
-int chiffres;
+int impair;//?
+int pair;//?
+int chiffres;//?
 
 int llettres[]={0,0,0,0,0,0,0,0};
 int lvoyelles[]={0,0,0,0,0,0,0,0};
@@ -69,20 +70,20 @@ int voyelles;
 int consonnes;
 
 char code[]="00000000";
+// valeur des 3 LED
 int NSA;
 int MSA;
 int FRK;
+//pin des 3 LED
 int ledNSA=11;
 int ledMSA=10;
 int ledFRK=9;
 
-int checkErreur()
-{
-for(i=0;i<5;i++)
-{
-  if( !(resultats[i]==combinaison[i]) ){ return 1; }
-}
-return 0;
+int checkErreur() {
+  for(i=0;i<5;i++) {
+    if( !(resultats[i]==combinaison[i]) ){ return 1; }
+  }
+  return 0;
 }
 
 
@@ -126,14 +127,14 @@ int condE4(){
     else{ return 0; }
 }
 
+// possibillite de completement enleve les "un","deux",...
 int chiffreE4(){
     int un=0;
     int deux=0;
     int trois=0;
     int quatre=0;
     int cinq=0;
-    for(i=0;i<5;i++)
-    {
+    for(i=0;i<5;i++) {
         if (combinaison[i]==1) {un=1;}
         else if (combinaison[i]==2) {deux=1;}
         else if (combinaison[i]==3) {trois=1;}
@@ -151,17 +152,19 @@ int dans(char num[], char liste[]){
   for(i=0;i<8;i++){
     if(liste[i]==num[0]){return 1;}
   }
-  return 0;}
+  return 0;
+}
 
 int cdans(int num, int liste[]){
   for(i=0;i<8;i++){
     if(liste[i]==num){return 1;}
   }
-  return 0;}
+  return 0;
+}
 
 int pasbranche(int n){
     if( cdans(n,combinaison)==1 ){ return 0; }
-  else{ return 1; }
+    else{ return 1; }
 }
 
 int addition(){
@@ -179,8 +182,7 @@ int libre(){
   return lib;
 }
               
-void gencombinaison()
-{
+void gencombinaison() {
   if( lchiffres[2]==1 ){ combinaison[0]=4 ;}
   else if( NSA==1 && llettres[6]==1 ){ combinaison[0]=3 ;}
   else if( dans("2",code)==1 ){ combinaison[0]=5 ;}
@@ -210,45 +212,44 @@ void gencombinaison()
 
 }
 
-
-void setup()
-{  
-  lcd.begin(16,2);
-  lcd.setRGB(colorR, colorG, colorB);
-  lcd.setCursor(0,0);
-  lcd.print("amogus");
+// fonction execute au lancement du programme
+void setup() {  
+    lcd.begin(16,2);
+    lcd.setRGB(colorR, colorG, colorB);
+    lcd.setCursor(0,0);
+    lcd.print("amogus");
+    
+    randomSeed(analogRead(9));
+    NSA=random()%2;
+    MSA=random()%2;
+    FRK=random()%2;
+    pinMode(ledNSA,OUTPUT);
+    pinMode(ledMSA,OUTPUT);
+    pinMode(ledFRK,OUTPUT);
+    
+    if(NSA==1){analogWrite(ledNSA,30);}
+    if(MSA==1){analogWrite(ledMSA,30);}
+    if(FRK==1){analogWrite(ledFRK,30);}
   
-  randomSeed(analogRead(9));
-  NSA=random()%2;
-  MSA=random()%2;
-  FRK=random()%2;
-  pinMode(ledNSA,OUTPUT);
-  pinMode(ledMSA,OUTPUT);
-  pinMode(ledFRK,OUTPUT);
+    pinMode(51,OUTPUT);
+    pinMode(53,INPUT);
+    
+    creecode();   
+    gencombinaison();
+    digitalWrite(51,HIGH);
+    delay(10);
+    digitalWrite(51,LOW);
   
-  if(NSA==1){analogWrite(ledNSA,30);}
-  if(MSA==1){analogWrite(ledMSA,30);}
-  if(FRK==1){analogWrite(ledFRK,30);}
-
-  pinMode(51,OUTPUT);
-  pinMode(53,INPUT);
   
-  creecode();   
-  gencombinaison();
-  digitalWrite(51,HIGH);
-  delay(10);
-  digitalWrite(51,LOW);
-
-
-
   
+    
     pinMode(bouton,INPUT);
     pinMode(cable1,INPUT);
     pinMode(cable2,INPUT);
     pinMode(cable3,INPUT);
     pinMode(cable4,INPUT);
     pinMode(cable5,INPUT);
-
+  
     lcd.print(code);
     lcd.setCursor(0,1);
     lcd.print(combinaison[0]);
@@ -257,95 +258,87 @@ void setup()
     lcd.print(combinaison[3]);
     lcd.print(combinaison[4]);
 
-  while(erreur<3 && victoire==0)
-  {
-      while(analogRead(bouton)<800)
-      {
-      perdu=digitalRead(40);
-      if(perdu==1){break;}
-      }
+    while(erreur<3 && victoire==0) {
+        while(analogRead(bouton)<800) {
+          perdu=digitalRead(40);
+          if(perdu==1){break;}
+        }
+  
+        if(perdu==1){break;}
+        float V1=analogRead(cable1)*5.0/1023.0;
+        float V2=analogRead(cable2)*5.0/1023.0;
+        float V3=analogRead(cable3)*5.0/1023.0;
+        float V4=analogRead(cable4)*5.0/1023.0;
+        float V5=analogRead(cable5)*5.0/1023.0;
+        resultats[0]=numero(V1);
+        resultats[1]=numero(V2);
+        resultats[2]=numero(V3);
+        resultats[3]=numero(V4);
+        resultats[4]=numero(V5);
+        
+        lcd.setCursor(0,1);
+        lcd.print(combinaison[0]);
+        lcd.print(combinaison[1]);
+        lcd.print(combinaison[2]);
+        lcd.print(combinaison[3]);
+        lcd.print(combinaison[4]);
+        
+        lcd.setCursor(6,1);
+  
+        if (checkErreur()==1){ erreur++; }
+        else { victoire=1; }
+        
+        if(erreur==1){
+          colorR = 255;
+          colorG = 171;
+          colorB = 0;
+          lcd.setRGB(colorR, colorG, colorB);
+          lcd.setCursor(11,0);
+          lcd.print("X");
+          lcd.print("X");
+          lcd.setCursor(11,1);
+          lcd.print("X");
+          lcd.print("X");
+        }
+        if(erreur==2){
+            colorR = 226;
+            colorG = 53;
+            colorB = 0;
+            lcd.setRGB(colorR, colorG, colorB);
+            lcd.setCursor(14,0);
+            lcd.print("X");
+            lcd.print("X");
+            lcd.setCursor(14,1);
+            lcd.print("X");
+            lcd.print("X");
+        }
+         
+        while(analogRead(bouton)>900) {
+            perdu=digitalRead(40);
+            if(perdu==1){break;}
+        }
+    }
 
-      
-      if(perdu==1){break;}
-      float V1=analogRead(cable1)*5.0/1023.0;
-      float V2=analogRead(cable2)*5.0/1023.0;
-      float V3=analogRead(cable3)*5.0/1023.0;
-      float V4=analogRead(cable4)*5.0/1023.0;
-      float V5=analogRead(cable5)*5.0/1023.0;
-      resultats[0]=numero(V1);
-      resultats[1]=numero(V2);
-      resultats[2]=numero(V3);
-      resultats[3]=numero(V4);
-      resultats[4]=numero(V5);
-      
-    lcd.setCursor(0,1);
-    lcd.print(combinaison[0]);
-    lcd.print(combinaison[1]);
-    lcd.print(combinaison[2]);
-    lcd.print(combinaison[3]);
-    lcd.print(combinaison[4]);
-      
-      lcd.setCursor(6,1);
 
-      if (checkErreur()==1){
-        erreur++;}
-      else { victoire=1;}
-      
-      if(erreur==1){
-        colorR = 255;
-        colorG = 171;
+    if (erreur==3 || perdu==1) {  
+        colorR = 0;
+        colorG = 0;
         colorB = 0;
-        lcd.setRGB(colorR, colorG, colorB);
-        lcd.setCursor(11,0);
-        lcd.print("X");
-        lcd.print("X");
-        lcd.setCursor(11,1);
-        lcd.print("X");
-        lcd.print("X");
-      }
-      if(erreur==2){
-        colorR = 226;
-        colorG = 53;
-        colorB = 0;
-        lcd.setRGB(colorR, colorG, colorB);
-        lcd.setCursor(14,0);
-        lcd.print("X");
-        lcd.print("X");
-        lcd.setCursor(14,1);
-        lcd.print("X");
-        lcd.print("X");
-      }
-       
-      while(analogRead(bouton)>900)
-      {
-      perdu=digitalRead(40);
-      if(perdu==1){break;}
-      }
-  }
-
-
-if(erreur==3 || perdu==1){  
-  colorR = 0;
-  colorG = 0;
-  colorB = 0;
-  lcd.setRGB(colorR, colorG, colorB);  
-  lcd.setCursor(0,0);
-  lcd.print("     Perdu      ");
-  lcd.setCursor(0,1);
-  lcd.print(" Recommencer ?  ");}
- 
-
-else{  
-  lcd.setCursor(0,0);
-  lcd.print("  Desamorcage   ");
-  lcd.setCursor(0,1);
-  lcd.print("    Reussi      ");}
-
-digitalWrite(51,LOW);
-
-}
+        lcd.setRGB(colorR, colorG, colorB);// pourquoi ne pas juste mettre 0 partout ? 
+        lcd.setCursor(0,0);
+        lcd.print("     Perdu      ");
+        lcd.setCursor(0,1);
+        lcd.print(" Recommencer ?  ");
+    } else {  
+        lcd.setCursor(0,0);
+        lcd.print("  Desamorcage   ");
+        lcd.setCursor(0,1);
+        lcd.print("    Reussi      ");
+    }
+    
+    digitalWrite(51,LOW);
+ }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }

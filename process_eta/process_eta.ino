f74865d7914333a9e61d62346a416b6116cc91c3
CATS <11648807+CoAuToSe@users.noreply.github.com>
2021-09-29 15:58:04 +0200

    Update process_eta.ino

--- process_eta/process_eta.ino
+++ process_eta/process_eta.ino
@@ -1,8 +1,29 @@
+#include <Wire.h>                                            // appel des bibliotheques
+#include "rgb_lcd.h"
 
 void setup() {
-  
+  //initialisation du system général
+  // I2C ?
+  //initialisation des modules principaux (times et autres si nécessaire)
 }
 
+int state = 0;
+bool module_finished = false;
+
 void loop() {
-  
+    int seed = random() % 3;
+    while (!module_finished) {
+        switch (seed) {
+            case 0 : Module1();
+                break;
+            case 0 : Module1();
+                break;
+            case 0 : Module1();
+                break;
+            case 0 : Module1();
+                break;
+        }
+    }
+    module_finished = false;
+    state++;
 }

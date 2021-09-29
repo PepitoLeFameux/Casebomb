ad5ecf6a5b42ed37145b07b2a565d999c08e3b63
CATS <11648807+CoAuToSe@users.noreply.github.com>
2021-09-29 14:34:21 +0200

    syntax update

--- protoype/protoype.ino
+++ protoype/protoype.ino
@@ -14,36 +14,36 @@ int colorB = 255;
 rgb_lcd lcd; 
 
 //pin des cables
-int bouton=A8;
+int bouton = A8;
 //cable1 2.50V
 //cable2 3.00V
 //cable3 4.10V
 //cable4 4.50V
-//cable5 4.7V
-int cable1=A0;
-int cable2=A1;
-int cable3=A2;
-int cable4=A3;
-int cable5=A4;
+//cable5 4.70V
+int cable1 = A0;
+int cable2 = A1;
+int cable3 = A2;
+int cable4 = A3;
+int cable5 = A4;
 
-float V1,V2,V3,V4,V5=0;
+float V1,V2,V3,V4,V5 = 0;
 
 //ordre des câbles à brancher
 //affiche 42069 si ok sinon 666
-unsigned int OK=42069;
+unsigned int OK = 42069;
 
 //variables d'etat du jeu
-int erreur=0;
-int victoire=0;
-int perdu=0;
+int erreur = 0;
+int victoire = 0;
+int perdu = 0;
 
 int numero(float tension) {
-    if (tension<0.1) { return 0; }
-    if (tension<0.3) { return 5; }
-    if (tension<0.7) { return 4; }
-    if (tension<1.5) { return 3; }
-    if (tension<2.0) { return 2; }
-    if (tension<3.0) { return 1; }
+    if (tension < 0.1) { return 0; }
+    if (tension < 0.3) { return 5; }
+    if (tension < 0.7) { return 4; }
+    if (tension < 1.5) { return 3; }
+    if (tension < 2.0) { return 2; }
+    if (tension < 3.0) { return 1; }
     else { return 9; }
 }
 
@@ -68,7 +68,7 @@ int lettres;
 int voyelles;//? pas utilise
 int consonnes;//? pas utilise
 
-char code[]="00000000";
+char code[] = "00000000";
 // valeur des 3 LED
 int NSA;
 int MSA;
@@ -79,23 +79,23 @@ int ledMSA = 10;
 int ledFRK = 9;
 
 int checkErreur() {
-    for(i = 0; i<5; i++) {
-        if (resultats[i]!=combinaison[i]) { return 1; }
+    for(i = 0; i < 5; i++) {
+        if (resultats[i] != combinaison[i]) { return 1; }
     }
     return 0;
 }
 
 int n;
 void creecode(){
-    for(i=0;i<8;i++){
+    for (i = 0; i < 8; i++){
         n = random()%36;
         code[i] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[n];
   
-        if (0<=n && n<=25) {
+        if (0 <= n && n <= 25) {
             llettres[i] = 1;
             lettres++;
     
-            if (n==0 || n==4 || n==8 || n==14 || n==20 || n==24){
+            if (n == 0 || n == 4 || n == 8 || n == 14 || n == 20 || n == 24){
                 lvoyelles[i] = 1;
                 voyelles++;
             } else {
@@ -117,8 +117,8 @@ void creecode(){
 }
 
 int condE4(){
-    if (pasbranche(5)==1) { return 5; }
-    if (pasbranche(1)==1) { return 1; }
+    if (pasbranche(5) == 1) { return 5; }
+    if (pasbranche(1) == 1) { return 1; }
     else { return 0; }
 }
 
@@ -144,44 +144,44 @@ int chiffreE4(){
 }
 
 int remplace_chiffreE4() {
-    for(i=0;i<5;i++) {
-        if      (combinaison[i]==1) { return 1; }
-        else if (combinaison[i]==2) { return 2; }
-        else if (combinaison[i]==3) { return 3; }
-        else if (combinaison[i]==4) { return 4; }
-        else if (combinaison[i]==5) { return 5; }
+    for (i = 0; i < 5; i++) {
+        if      (combinaison[i] == 1) { return 1; }
+        else if (combinaison[i] == 2) { return 2; }
+        else if (combinaison[i] == 3) { return 3; }
+        else if (combinaison[i] == 4) { return 4; }
+        else if (combinaison[i] == 5) { return 5; }
     }
 }
 
 int dans(char num[], char liste[]){
-    for(i=0;i<8;i++){
-      if(liste[i]==num[0]){return 1;}
+    for (i = 0; i < 8; i++) {
+      if (liste[i] == num[0]) { return 1; }
     }
     return 0;
 }
 
 int cdans(int num, int liste[]) {
-    for (i=0; i<8; i++){
-        if (liste[i]==num) {return 1;}
+    for (i = 0; i < 8; i++){
+        if (liste[i] == num) { return 1; }
     }
     return 0;
 }
 
 int pasbranche(int n) {
-    if (cdans(n,combinaison) == 1){ return 0; }
+    if (cdans(n,combinaison) == 1) { return 0; }
     else { return 1; }
 }
 
 int addition() {
     int add = 0;
-    for (i=0; i<5; i++) {
-        if (combinaison[i]==0) { add = add + i + 1; }
+    for (i = 0; i < 5; i++) {
+        if (combinaison[i] == 0) { add = add + i + 1; }
     }
 }
 
 int libre() {
     int lib = 0;
-    for (i=0; i<5; i++) {
+    for (i = 0; i < 5; i++) {
         if (combinaison[i] == 0) { lib++; }
     }
     return lib;
@@ -189,7 +189,7 @@ int libre() {
               
 void gencombinaison() {
     if (lchiffres[2] == 1 ) { combinaison[0] = 4; }
-    else if (NSA==1 && llettres[6] == 1 ) { combinaison[0] = 3; }
+    else if (NSA == 1 && llettres[6] == 1 ) { combinaison[0] = 3; }
     else if (dans("2",code) == 1 ) { combinaison[0] = 5; }
     else { combinaison[0] = 4; }
   
@@ -197,19 +197,19 @@ void gencombinaison() {
     else if (lettres >= 4 && pasbranche(3) == 1 ) { combinaison[1] = 3; }
     else if (dans("2",code) == 1 && dans("4",code) == 1 && pasbranche(5) == 1) { combinaison[1] = 5; }
     else { combinaison[1] = 2; }
-  
+
     if (pasbranche(5) == 1 && dans("V",code)) { combinaison[2] = 5; }
     else if (pasbranche(1) == 1 && lettres >= 5) { combinaison[2] = 1; }
     else if (llettres[3] == "1" && lchiffres[6] == "1" && pasbranche(2) == 1) { combinaison[2] = 2; }
     else if (pasbranche(3) == 1) { combinaison[2] = 3; }
     else { combinaison[2] = 0; }
-  
+
     if (dans("D",code) && dans("E",code) && pasbranche(4) == 1) { combinaison[3] = 4; }
     else if (FRK == 1 && MSA == 1 && pasbranche(2) == 1) { combinaison[3] = 2; }
     else if (addition() >= 7 && pasbranche(1) == 1) { combinaison[3] = 1; }
     else if (dans("0",code) == 1 && dans("5",code) == 1 && pasbranche(5)) { combinaison[3] = 5; }
     else { combinaison[3] = 0; }
-  
+
     if (libre() > 2) { combinaison[4] = 0; }
     else if (cdans(1,combinaison) == 1 && cdans(3,combinaison) == 1 && cdans(5,combinaison) == 1) { combinaison[4] = 4; }
     else if (condE4() == 1 || condE4() == 5 ) { combinaison[4] = condE4(); }
@@ -265,7 +265,7 @@ void setup() {
             if (perdu == 1) { break; }
         }
   
-        if (perdu == 1){ break; }
+        if (perdu == 1) { break; }
         float V1 = analogRead(cable1)*5.0/1023.0;
         float V2 = analogRead(cable2)*5.0/1023.0;
         float V3 = analogRead(cable3)*5.0/1023.0;
@@ -318,7 +318,7 @@ void setup() {
             if (perdu == 1) { break; }
         }
     }
-    if (erreur==3 || perdu==1) {  
+    if (erreur == 3 || perdu == 1) {
         colorR = 0;
         colorG = 0;
         colorB = 0;
@@ -327,7 +327,7 @@ void setup() {
         lcd.print("     Perdu      ");
         lcd.setCursor(0,1);
         lcd.print(" Recommencer ?  ");
-    } else {  
+    } else {
         lcd.setCursor(0,0);
         lcd.print("  Desamorcage   ");
         lcd.setCursor(0,1);
@@ -335,7 +335,7 @@ void setup() {
     }
     
     digitalWrite(51,LOW);
- }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:

a1780cb6ee9b7750146c8faf09839e5bb553217b
CATS <11648807+CoAuToSe@users.noreply.github.com>
2021-09-29 16:11:30 +0200

    Update process_eta.ino

--- process_eta/process_eta.ino
+++ process_eta/process_eta.ino
@@ -1,7 +1,21 @@
-#include <Wire.h>                                            // appel des bibliotheques
-#include "rgb_lcd.h"
+// #include <Wire.h>                                            // appel des bibliotheques
+// #include "rgb_lcd.h"
+
+void Module1() {
+    
+}
+void Module2() {
+    
+}
+void Module3() {
+    
+}
+void Module4() {
+    
+}
 
 void setup() {
+  randomSeed(analogRead(0));
   //initialisation du system général
   // I2C ?
   //initialisation des modules principaux (times et autres si nécessaire)
@@ -10,18 +24,28 @@ void setup() {
 int state = 0;
 bool module_finished = false;
 
+int seed;
 void loop() {
-    int seed = random() % 3;
+    seed = random(10) % 3;
     while (!module_finished) {
-        switch (seed) {
-            case 0 : Module1();
+        switch (true) {
+            case 0 : {
+                Module1();
                 break;
-            case 0 : Module1();
+            }
+            case 1 :{
+                Module2();
                 break;
-            case 0 : Module1();
+            }
+            case 2 : {
+                Module3();
                 break;
-            case 0 : Module1();
+            }
+            case 3 : {
+                Module4();
                 break;
+            }
+            default: { break; }
         }
     }
     module_finished = false;

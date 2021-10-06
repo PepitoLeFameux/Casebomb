35abea80174aab0574b0d488f0323b621080ed29
PepitoLeFameux <84035616+PepitoLeFameux@users.noreply.github.com>
2021-10-06 17:30:58 +0200

    gencombinaison
    Explication détaillée de la fonction gencombinaison

--- protoype/protoype.ino
+++ protoype/protoype.ino
@@ -198,41 +198,63 @@ int libre() {
 
 //la combinaison correcte est générée grâce aux conditions aléatoires et aux règles du jeu
 void gencombinaison() {
+      //CABLE A
+    //si chiffre en 3eme position -> branche au 4
     if (lchiffres[2] == 1 ) { combinaison[0] = 4; }
+    //si NSA allumée et lettre en 7eme position -> branche au 3
     else if (NSA == 1 && llettres[6] == 1 ) { combinaison[0] = 3; }
+    //si le code comporte un 2 -> branche au 5
     else if (dans("2",code) == 1 ) { combinaison[0] = 5; }
+    //sinon branche au 4
     else { combinaison[0] = 4; }
-  
+
+      //CABLE B
+    //si MSA allumée -> branche au 1
     if (MSA == 1) { combinaison[1]=1 ;}
-    else if (lettres >= 4 && pasbranche(3) == 1 ) { combinaison[1] = 3; }
+    //si plus de 3 lettres et port 3 vide -> branche au 3    
+    else if (lettres > 3 && pasbranche(3) == 1 ) { combinaison[1] = 3; }
+    //si le code comporte 2 et 4 et que port 5 vide -> branche au 5
     else if (dans("2",code) == 1 && dans("4",code) == 1 && pasbranche(5) == 1) { combinaison[1] = 5; }
+    //sinon branche au 2
     else { combinaison[1] = 2; }
 
+      //CABLE C
+    //si port 5 vide et le code comporte un V -> branche au 5
     if (pasbranche(5) == 1 && dans("V",code)) { combinaison[2] = 5; }
-    else if (pasbranche(1) == 1 && lettres >= 5) { combinaison[2] = 1; }
+    //si port 1 vide et plus de 4 lettres dans le code -> branche au 1
+    else if (pasbranche(1) == 1 && lettres > 4) { combinaison[2] = 1; }
+    //si lettre en 4eme position et chiffre en 7eme position et port 2 vide -> branche au 2
     else if (llettres[3] == "1" && lchiffres[6] == "1" && pasbranche(2) == 1) { combinaison[2] = 2; }
+    //si port 3 vide -> branche au 3
     else if (pasbranche(3) == 1) { combinaison[2] = 3; }
+    //sinon ne pas brancher
     else { combinaison[2] = 0; }
 
+      //CABLE D
+    //si le code comporte un D, un E et que le port 4 est vide -> branche au 4
     if (dans("D",code) && dans("E",code) && pasbranche(4) == 1) { combinaison[3] = 4; }
+    //si FRK et MSA allumé et port 2 vide -> branche au 2
     else if (FRK == 1 && MSA == 1 && pasbranche(2) == 1) { combinaison[3] = 2; }
-    else if (addition() >= 7 && pasbranche(1) == 1) { combinaison[3] = 1; }
+    //si l'addition des numéros des ports vides est supérieure à 6 et port 1 vide -> branche au 1
+    else if (addition() > 6 && pasbranche(1) == 1) { combinaison[3] = 1; }
+    //si le code comporte un 0 (zéro) et un 5 et port 5 vide -> branche au 5
     else if (dans("0",code) == 1 && dans("5",code) == 1 && pasbranche(5)) { combinaison[3] = 5; }
+    //sinon ne pas brancher
     else { combinaison[3] = 0; }
 
-    if (libre() > 2) { combinaison[4] = 0; }
+      //CABLE E
+    //si plus de 1 port vides -> ne branche pas
+    if (libre() > 1) { combinaison[4] = 0; }
+    //si les ports 1,3,5 sont occupés -> branche au 4
     else if (cdans(1,combinaison) == 1 && cdans(3,combinaison) == 1 && cdans(5,combinaison) == 1) { combinaison[4] = 4; }
+    //si le port 5 est vide -> branche au 5, sinon si le 1 est vide -> branche au 1
     else if (condE4() == 1 || condE4() == 5 ) { combinaison[4] = condE4(); }
+    //si le code comporte un B et un P -> branche au dernier port (le seul pas branché)
     else if (dans("B",code) == 1 && dans("P",code) == 1) { combinaison[4] = chiffreE4(); }
 }
 
 // fonction qui s'execute au lancement du programme
-<<<<<<< HEAD
-void setup() {  
-  //initialisation de l'écran et affichage 
-=======
 void setup() {
->>>>>>> fd0e8d793a97d25e9a1e55cbc554d2f212901efc
     lcd.begin(16,2);
     lcd.setRGB(colorR, colorG, colorB);
     lcd.setCursor(0,0);

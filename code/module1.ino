c04e6f34a84800183cf2b7f90d4256737de5839a
CATS <11648807+CoAuToSe@users.noreply.github.com>
2021-11-17 15:06:01 +0100

    Update module1.ino

--- code/module1.ino
+++ code/module1.ino
@@ -93,7 +93,7 @@ void gencombinaison(int combinaison[], int llettres[], int lchiffres[]) {
     //si NSA allumée et lettre en 7eme position -> branche au 3
     else if (NSA == 1 || FRK == 1 && llettres[6] == 1 ) { combinaison[0] = 3; }
     //si le code comporte un 2 -> branche au 5
-    else if (dans("2",code) == 1 ) { combinaison[0] = 5; }
+    else if (dans("2", code) == 1 ) { combinaison[0] = 5; }
     //sinon branche au 4
     else { combinaison[0] = 4; }
 
@@ -103,13 +103,13 @@ void gencombinaison(int combinaison[], int llettres[], int lchiffres[]) {
     //si plus de 3 lettres et port 3 vide -> branche au 3    
     else if (lettres > 3 && pasbranche(3) == 1 ) { combinaison[1] = 3; }
     //si le code comporte 2 et 4 et que port 5 vide -> branche au 5
-    else if (dans("2",code) == 1 && dans("4",code) == 1 && pasbranche(5) == 1) { combinaison[1] = 5; }
+    else if (dans("2", code) == 1 && dans("4", code) == 1 && pasbranche(5) == 1) { combinaison[1] = 5; }
     //sinon branche au 2
     else { combinaison[1] = 2; }
 
       //CABLE C
     //si port 5 vide et le code comporte un V -> branche au 5
-    if (pasbranche(5) == 1 && dans("V",code)) { combinaison[2] = 5; }
+    if (pasbranche(5) == 1 && dans("V", code)) { combinaison[2] = 5; }
     //si port 1 vide et plus de 4 lettres dans le code -> branche au 1
     else if (pasbranche(1) == 1 && lettres > 4) { combinaison[2] = 1; }
     //si lettre en 4eme position et chiffre en 7eme position et port 2 vide -> branche au 2
@@ -121,13 +121,13 @@ void gencombinaison(int combinaison[], int llettres[], int lchiffres[]) {
 
       //CABLE D
     //si le code comporte un D, un E et que le port 4 est vide -> branche au 4
-    if (dans("D",code) || dans("E",code) || dans("T",code) && pasbranche(4) == 1) { combinaison[3] = 4; }
+    if (dans("D", code) || dans("E", code) || dans("T", code) && pasbranche(4) == 1) { combinaison[3] = 4; }
     //si FRK et MSA allumé et port 2 vide -> branche au 2
     else if (FRK == 1 && pasbranche(2) == 1) { combinaison[3] = 2; }
     //si l'addition des numéros des ports vides est supérieure à 6 et port 1 vide -> branche au 1
     else if (addition() > 6 && pasbranche(1) == 1) { combinaison[3] = 1; }
     //si le code comporte un 0 (zéro) et un 5 et port 5 vide -> branche au 5
-    else if (dans("0",code) == 1 && dans("5",code) == 1 && pasbranche(5)) { combinaison[3] = 5; }
+    else if (dans("0", code) == 1 && dans("5", code) == 1 && pasbranche(5)) { combinaison[3] = 5; }
     //sinon ne pas brancher
     else { combinaison[3] = 0; }
 
@@ -139,7 +139,7 @@ void gencombinaison(int combinaison[], int llettres[], int lchiffres[]) {
     //si le port 5 est vide -> branche au 5, sinon si le 1 est vide -> branche au 1
     else if (condE4() == 1 || condE4() == 5 ) { combinaison[4] = condE4(); }
     //si le code comporte un B et un P -> branche au dernier port (le seul pas branché)
-    else if (dans("B",code) == 1 && dans("P",code) == 1) { combinaison[4] = chiffreE4(); }
+    else if (dans("B", code) == 1 && dans("P", code) == 1) { combinaison[4] = chiffreE4(); }
 }
 
 void button_pressed() {

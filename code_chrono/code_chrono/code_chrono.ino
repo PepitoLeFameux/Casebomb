e5ea7dfa4797eddf7b34cac0d6a0b092e4ee4dd8
CarlLinnaeus <91207413+CarlLinnaeus@users.noreply.github.com>
2021-11-24 15:22:40 +0100

    Update code_chrono.ino

--- code_chrono/code_chrono/code_chrono.ino
+++ code_chrono/code_chrono/code_chrono.ino
@@ -3,8 +3,8 @@
 #include "rgb_lcd.h"
 
 
-int SlaveReceived = 0;
-int SlaveSend = 0;
+int SlaveReceived = 12;
+int SlaveSend = 5;
 
 int colorR = 0;                                    
 int colorG = 255;
@@ -13,7 +13,7 @@ int colorB = 0;
 unsigned long tempsDepart;
 unsigned long temps;
 unsigned long tempsEcoule;
-unsigned long tempsMax=30000; //300000 ms = 5 min
+unsigned long tempsMax=10000; //300000 ms = 5 min
 int secondeAvant=0;
 
 bool fini=false;
@@ -21,6 +21,8 @@ bool fini=false;
 int minutes;
 int secondes;
 int led=9;
+
+int depart=0;
 rgb_lcd lcd; 
 
 void receiveEvent (int combien)      //Fonction appelée lorsque l'esclave recoit un signal    
@@ -31,30 +33,35 @@ void receiveEvent (int combien)      //Fonction appelée lorsque l'esclave recoi
 void requestEvent ()     // fonction appelée lorsque l'esclave envoie un signal
 {
   Wire.write(SlaveSend);
-}
+  }
+
 
 
 void setup() {
   Wire.begin(8);
-
+  Wire.setClock(100000);
   lcd.begin(16, 2);
   lcd.setRGB(colorR, colorG, colorB);
   lcd.setCursor(0,0);
   lcd.print("Compte a rebours");
   //pinMode(A0,INPUT);
   Wire.onReceive(receiveEvent);
-  tempsDepart = millis();
+  Wire.onRequest(requestEvent);
 }
 
 void loop() 
 { 
+  
 
 //Valeur recue est 20 si victoire de la part de la maitre,10 pour une défaite et 50 pour départ chrono
 //Valeur envoyée 10 quand chrono fini
 
 
-
   if(SlaveReceived==50 && fini==false){
+    if(depart==0){      
+      tempsDepart = millis();
+      depart=1;
+    }
     temps=millis();
     tempsEcoule=tempsMax-(temps-tempsDepart);
     minutes=tempsEcoule/60000; //minutes écoulées avec 60000 ms
@@ -65,7 +72,7 @@ void loop()
     else{lcd.print(0);lcd.print(minutes);}
     lcd.print(":");
     if(secondes>9){lcd.print(secondes);}
-    else{lcd.print(0);lcd.print(secondes);}
+    else{lcd.print(0);lcd.print(secondes + "           ");}
     
     
     if(!(colorG==0 && colorR==255) && secondeAvant!=secondes) {
@@ -78,7 +85,6 @@ void loop()
     if(minutes==0&&secondes==0){
       fini=true;
       SlaveSend = 10;
-      Wire.onRequest(requestEvent);
     }
   }
   
@@ -88,6 +94,7 @@ void loop()
 
   
   if (SlaveReceived==10 || fini==true){             //affiche message de defaite
+    SlaveSend = 10;
     lcd.setRGB(255,0,0);
     lcd.setCursor(0,0);
     lcd.print("      Perdu      ");
